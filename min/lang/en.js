b6c4a6300decff3ac73ea86c40d54c69fa10ef26
Craig Silverstein <csilvers@khanacademy.org>
2013-06-10 17:29:05 -0700

    Change en.js to be an empty file, rather than pointing to en-ca.js.
    Summary:
    This means the default khanacademy site (with no lang= parameter) will
    use the default moment-js strings, which is what we want since those
    default strings are for en-US.
    
    Test Plan:
    Ran a dev-appserver and logged in as a user who coaches a student, and
    visited
       localhost:8080/class_profile/progress-report
    I clicked on the 'all time' dropdown in the top right, and selected
    custom range', and saw that the range said 'Start: June 9 2013',
    whereas before this change it said 'Start: 9 June, 2013'.
    
    Reviewers: joshnetterfield, chris, khirasaki
    
    Reviewed By: khirasaki
    
    CC: james
    
    Differential Revision: http://phabricator.khanacademy.org/D2623

--- min/lang/en.js
+++ min/lang/en.js
@@ -1,1 +1,13 @@
-en-ca.js
+// moment.js language configuration
+// language : default (US english)
+//
+// Since en-US is the default language used by the moment library, we
+// don't need to have any content here.  This file is just a
+// placeholder indicating the user wants US English.
+//
+// We need this file because inject_i18n_strings causes
+// __language__.js to point to en.js for unsupported languages (such
+// as 'boxes'), so en.js must always exist in this directory.
+//
+// Maybe the right fix is to change inject_i18n_strings.py to point
+// somewhere else if en.js doesn't exist, but this fix is easier.
